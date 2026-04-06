3144ce7960b4f180c4701f1d5d0cfdfe52709d0f
fibin <fibinkunnath@gmail.com>
2026-04-06 12:24:39 +0530

    f

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const connectDB = require("./config/db");
+const adminUserRoutes = require("./routes/adminUserRoutes");
+
+
+const app = express();
+
+connectDB();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://e-comercer-frontend.vercel.app",
@@ -14,3 +27,24 @@ app.use(cors({
   },
   credentials: true,
 }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/wishlist", require("./routes/wishlistRoutes"));
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.get("/", (req, res) => {
+  res.send("🚀 API Running");
+});
+app.use("/api/admin", adminUserRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🔥 Server running on port ${PORT}`);
+});
