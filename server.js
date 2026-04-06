60b51ac03122634491200eda7957df911c808449
fibin <fibinkunnath@gmail.com>
2026-04-06 12:21:26 +0530

    e

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const connectDB = require("./config/db");
-const adminUserRoutes = require("./routes/adminUserRoutes");
-
-
-const app = express();
-
-connectDB();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-comercer-frontend.vercel.app",
+  "https://solesociety-6114e04of-fibins-projects-3db35d72.vercel.app"
+];
 
 app.use(cors({
-  origin: process.env.VITE_API_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS blocked: " + origin));
+    }
+  },
   credentials: true,
 }));
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/cart", require("./routes/cartRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
-app.use("/api/wishlist", require("./routes/wishlistRoutes"));
-
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.get("/", (req, res) => {
-  res.send("🚀 API Running");
-});
-app.use("/api/admin", adminUserRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
-});
